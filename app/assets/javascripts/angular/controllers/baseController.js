726b7a12bd06430496254887b4e665c4d6f812c5
Jason <devilshaircut@gmail.com>
2017-02-08 13:04:45 -0500

    Delete whitespace.

--- app/assets/javascripts/angular/controllers/baseController.js
+++ app/assets/javascripts/angular/controllers/baseController.js
@@ -1,13 +1,5 @@
 AngularApp.controller("baseController", ["$rootScope", "$scope", "httpService", function($rootScope, $scope, httpService) {
 
-
-
-
-
-
-  console.log("baseController:");
-
-
   $scope.form = [];
 
   $scope.formMethods = {
@@ -110,31 +102,20 @@ AngularApp.controller("baseController", ["$rootScope", "$scope", "httpService", 
     }
   };
 
+  // // This object basically serves as the basic data model.
+  // // It is set when the main page AJAX request completes successfully.
+  // $scope.model = {};
 
+  // // These strings specify where the API endpoints for this view reside.
+  // var apiEndpoint = '/sampleResponse.json';
 
-  
+  // // This is the callback function that executes if the HTTP request for $scope.model returns successfully.
+  // var getModelSuccess = function(payload, status) { $scope.model = payload; };
+  // // This is the callback function that executes if the HTTP request for $scope.model returns unsuccessfully.
+  // var getModelFailure = function(payload, status) {};
 
-
-
-
-
-
-
-
-  // This object basically serves as the basic data model.
-  // It is set when the main page AJAX request completes successfully.
-  $scope.model = {};
-
-  // These strings specify where the API endpoints for this view reside.
-  var apiEndpoint = '/sampleResponse.json';
-
-  // This is the callback function that executes if the HTTP request for $scope.model returns successfully.
-  var getModelSuccess = function(payload, status) { $scope.model = payload; };
-  // This is the callback function that executes if the HTTP request for $scope.model returns unsuccessfully.
-  var getModelFailure = function(payload, status) {};
-
-  // Initiate the HTTP request.
-  httpService.getApiEndpoint(apiEndpoint).success(getModelSuccess);
+  // // Initiate the HTTP request.
+  // httpService.getApiEndpoint(apiEndpoint).success(getModelSuccess);
 
 }]);
 
